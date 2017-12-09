a2ab6bc498b526680287c28689e1164433d2c70b
Marek Kubicka <kubicka.marek@gmail.com>
2017-12-09 05:10:02 -0600

    WIP

---
+++ Assets/QuestionClick.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class QuestionClick : MonoBehaviour
+{
+
+	private GameObject _button;
+	// Use this for initialization
+	void Start () {
+		GameObject.FindGameObjectWithTag("questionButton").GetComponent<Button>().onClick.AddListener(DoIt);
+	}
+	
+	void DoIt()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
